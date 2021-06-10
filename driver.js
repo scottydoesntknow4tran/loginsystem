dc6a3ff948da452bed36386836e94994ff9e7be9
scottydoesntknow4tran <scottytornquist@hotmail.com>
2021-06-10 01:00:30 +0100

    rpi changes

--- driver.js
+++ driver.js
@@ -1,13 +1,13 @@
-// const Gpio = require('onoff').Gpio;
-// var RELAY = 4;
-// const led = new Gpio(RELAY, 'out');
+const Gpio = require('onoff').Gpio;
+var RELAY = 4;
+const led = new Gpio(RELAY, 'out');
 
-// console.log("Setup");
+console.log("Setup");
 	
 
 function pulse_relay(){
-//     led.write(1);
-//     setTimeout(() => {led.write(0);}, 1000);
+     led.write(1);
+     setTimeout(() => {led.write(0);}, 1000);
     console.log("door toggled");
 };
 
